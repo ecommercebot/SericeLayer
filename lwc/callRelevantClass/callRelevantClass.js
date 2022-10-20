f1c720f67681ec5ce670534b2d8159eddf0872be
Alexander Chernov <skype7678@gmail.com>
2022-10-20 11:20:00 +0300

    OK

--- lwc/callRelevantClass/callRelevantClass.js
+++ lwc/callRelevantClass/callRelevantClass.js
@@ -1,16 +1,14 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from 'lwc'
 import createTask from '@salesforce/apex/TaskController.createTask'
 
 export default class CallRelevantClass extends LightningElement {
-    @api recordId;
+    @api recordId
 
     handleClick() {
-
-        createTask({"id": this.recordId}).then(result=>{
+        createTask( {"id": this.recordId} ).then(result => {
 			console.log(result)
-		}).catch(error=>{
+		}).catch(error => {
             console.log( JSON.stringify(error ) )
-		});
-
+		})
     }
 }
